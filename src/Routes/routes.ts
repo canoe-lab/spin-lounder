66f67524055cdd9c38db77669e209fed3ff9d8fb
Denys Olianych <dneys.olianych.com>
2024-07-16 19:07:59 -0400

    Update route paths for homepage and creator token mint page

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,2 +1,2 @@
-export const HOME = "/spin-lounder";
-export const CREATOR_TOKEN_MINT = "/spin-lounder/creator-token-mint";
+export const HOME = "/";
+export const CREATOR_TOKEN_MINT = "/creator-token-mint";
