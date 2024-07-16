d2f9c7f2d9d523453f3f7d4c24588b6929abfb06
Denys Olianych <dneys.olianych.com>
2024-07-16 18:26:24 -0400

    Update route paths for homepage and creator token mint page

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router basename="/">
+    <Router basename="/spin-lounder">
       <App />
     </Router>
   </React.StrictMode>
